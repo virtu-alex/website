c8b6a7cbef5608674ba26779fd38d22ad3eb263b
torone89 <torone89>
2022-09-25 12:26:33 +0200

    feat:add container

---
+++ src/components/TheFooter.vue
@@ -0,0 +1,13 @@
+<footer>
+<div class='container'>
+
+</div>
+</footer>
+
+<script>
+import { defineComponent } from "@vue/composition-api";
+
+export default defineComponent({
+  setup() {},
+});
+</script>
